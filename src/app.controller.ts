9ea3749f31b44e6206ed2e4f351730d04af414e1
Dante05S <Dante05S@hotmail.com>
2024-01-26 02:00:35 -0500

    :gear: improve response app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,10 @@
 import { Controller, Get, HttpStatus } from '@nestjs/common'
-import { Response } from './helpers/request'
+import { Response, http } from './helpers/request'
 
 @Controller()
 export class AppController {
   @Get()
   getApp(): Response<null> {
-    return {
-      data: null,
-      message: 'Welcome to To Do Core!',
-      code: HttpStatus.OK,
-      success: true,
-      errors: []
-    }
+    return http.response(null, HttpStatus.OK, 'Welcome to To Do Core!')
   }
 }
